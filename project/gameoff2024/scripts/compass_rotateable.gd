9ae9eda7f50cc2b5a6c8498ff8c49905e0508eac
Manisini <manisini.chakraborty@gmail.com>
2024-11-19 22:21:30 +0530

    Implemented compass

---
+++ project/gameoff2024/scripts/compass_rotateable.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var isPuzzleSolved: bool = false
+var isRotatedToOriginal: bool = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_player.play("default")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if isPuzzleSolved && !isRotatedToOriginal:
+		isRotatedToOriginal = true
+		animation_player.play("rotateToOriginal")
+
+
+func _on_playerdetector_body_entered(body: Node2D) -> void:
+	if body is Player:
+		isPuzzleSolved = true
